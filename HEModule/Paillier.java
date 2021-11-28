8f610c4552446fe5392fb2d99531ca5745a5f976
Pranav Prakasan <pranav.prakasan2019@gmail.com>
2021-11-28 14:02:33 +0530

    Cleanup

--- HEModule/Paillier.java
+++ HEModule/Paillier.java
@@ -51,24 +51,37 @@ public class Paillier {
         p = a;
         q = b;
 
+        // n = p*q
         n = p.multiply(q);
+
+        //n^2 = n*n
         nsquare = n.multiply(n);
 
+        // Choose random 'g'
         g = new BigInteger("2");
+
+        // lambda = lcm(p-1, q-1)
+        // lambda = p-1 * q-1 / gcd(p-1, q-1)
         lambda = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE)).divide(
                 p.subtract(BigInteger.ONE).gcd(q.subtract(BigInteger.ONE)));
 
+        // If not right, choose another
         if (g.modPow(lambda, nsquare).subtract(BigInteger.ONE).divide(n).gcd(n).intValue() != 1) {
             System.out.println("Message" + "g is not good. Choose g again.");
         }
     }
 
+    // Random r, 0 < r < n
     public BigInteger Encryption(BigInteger m, BigInteger r) {
+
+        // ciphertext c = ((g^m) * (r^n)) mod (n*n)
         return g.modPow(m, nsquare).multiply(r.modPow(n, nsquare)).mod(nsquare);
     }
 
     public BigInteger Decryption(BigInteger c) {
         BigInteger u = g.modPow(lambda, nsquare).subtract(BigInteger.ONE).divide(n).modInverse(n);
+
+        // plaintext m = (L * ((c^lambda) mod (n*n)) * mu mod n
         return c.modPow(lambda, nsquare).subtract(BigInteger.ONE).divide(n).multiply(u).mod(n);
     }
 
