c5d8492bc095f20bbe6ddd2ef7c447bfd9cdcd5d
Pranav Prakasan <pranav.prakasan2019@gmail.com>
2021-12-08 13:02:36 +0530

    Text compression

--- HEModule/Main.java
+++ HEModule/Main.java
@@ -1,20 +1,24 @@
 package HEModule;
 
+import HEModule.compression.Huffman;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.sql.Timestamp;
+import java.util.Scanner;
 
 class Main {
-    public static void wait(int ms)
-    {
-        try
-        {
+    public static void wait(int ms) {
+        try {
             Thread.sleep(ms);
-        }
-        catch(InterruptedException ex)
-        {
+        } catch (InterruptedException ex) {
             Thread.currentThread().interrupt();
         }
     }
+
     public static void main(String[] args) throws InterruptedException {
 
         String entered_text = "This is a sample text to illustrate HE";
@@ -48,7 +52,7 @@ class Main {
         System.out.println("\nStart Time : " + tsStart);
         System.out.println("Encrypted Message:" + encrypted);
 
-        execTime = (execTime/3);
+        execTime = (execTime / 3);
         Paillier p = new Paillier();
         p.encryptStringParallel(entered_text, BigInteger.ONE);
         tsEnd = new Timestamp(System.currentTimeMillis());
@@ -58,6 +62,38 @@ class Main {
         System.out.println("Execution Time: " + execTime + "ms");
 
         System.out.println("\nDecrypted Message:" + decrypted);
+
+        //Writing to file
+        System.out.println("Writing to file");
+        try {
+            FileWriter myWriter = new FileWriter("encrypted_text.txt");
+            myWriter.write(encrypted);
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        //Reading to file
+        System.out.println("Reading from file");
+        try {
+            // Create f1 object of the file to read data
+            File f1 = new File("encrypted_text.txt");
+            Scanner dataReader = new Scanner(f1);
+            while (dataReader.hasNextLine()) {
+                String fileData = dataReader.nextLine();
+                String compress = String.valueOf(Huffman.Compress(fileData, "compress.txt", "D://FALL SEMESTER 2021-22//A1 - DATA PRIVACY//JavaExec//HEModule"));
+                String decompress = String.valueOf(Huffman.Decompress("compress.huffman", "D://FALL SEMESTER 2021-22//A1 - DATA PRIVACY//JavaExec//HEModule"));
+                System.out.println(fileData);
+                System.out.println("Compressed:" + compress);
+                System.out.println("Decompressed:" + decompress);
+            }
+            dataReader.close();
+        } catch (FileNotFoundException exception) {
+            System.out.println("Unexpected error occurred!");
+            exception.printStackTrace();
+        }
 
     }
 }
